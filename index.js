89131c117acb64834c9707b5762f4c88d88014e3
Abdullah Naifeh <abdna59@gmail.com>
2022-10-17 16:04:28 +0300

    Complete assignment Test

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400){
+    return "This one is on me!"
+  }else if(feet === 400 || feet <= 2000){
+    return "That will be twenty bucks."
+  }else if(feet === 2000 || feet < 2500){
+    return "I will gladly take your thirty bucks."
+  } else if(feet > 2500){
+    return "No can do."
+  }
+}
+console.log(scuberGreetingForFeet(600))
+
+function ternaryCheckCity(city){
+  // Write your code here!
+   return city === "NYC" ? 'Ok, sounds good.' : 'No go.' ;
+}
+console.log(ternaryCheckCity("NYC"));
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip) {
+    case 'generous':
+return "Thank you so much."
+    case 'not as generous' :
+return "Thank you."
+    default:
+      return "Bye."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+console.log(switchOnCharmFromTip('not as generous'))
